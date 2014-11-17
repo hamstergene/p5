44c0075b26824681a0fd8e7d679c8a1aa87c33b5
Eugene Homyakov <ehomyakov@gmail.com>
2014-11-17 13:25:49 +0300

    added homebrew formula

---
+++ p5-brew-formula.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class P5BrewFormula < Formula
+  homepage "https://github.com/hamstergene/p5"
+  head "https://github.com/hamstergene/p5.git"
+
+  depends_on "python3"
+  depends_on "homebrew/binary/perforce"
+
+  def install
+    bin.install "p5" => "p5"
+  end
+end
+
